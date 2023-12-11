475bd2132afda62cb6f35787c54835513d7136fa
Nursultan <122966028+pp2-22B030488@users.noreply.github.com>
2023-12-11 11:08:01 +0600

    Update Admin.java (Nurik)

--- src/Packages/FromUser/Admin.java
+++ src/Packages/FromUser/Admin.java
@@ -1,58 +1,92 @@
-package Packages;
+package fromUser;
 
+import java.util.ArrayList;
+import java.util.Objects;
 
-/**
-* @generated
-*/
-public class Admin extends User {
-    
-    /**
-    * @generated
-    */
-    private User users;
-    
-    
-    
-    /**
-    * @generated
-    */
-    public User getUsers() {
-        return this.users;
+public class Admin extends User{
+	private ArrayList<User> users;
+	
+    public Admin(fromUser.UserType UserType, String ID, String FirstName, String LastName, String Email,
+			String Password, int PhoneNumber) {
+		super(UserType, ID, FirstName, LastName, Email, Password, PhoneNumber);
+		// TODO Auto-generated constructor stub
+	}
+    public Admin() {
+        users = new ArrayList<>();
+       
     }
-    
-    /**
-    * @generated
-    */
-    public User setUsers(User users) {
+
+    public Admin(ArrayList<User> users) {
         this.users = users;
     }
-    
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public boolean AddUser() {
-        //TODO
-        return false;
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public boolean addUser(User user) {
+        // Check if the user already exists before adding
+        if (!users.contains(user)) {
+            users.add(user);
+            return true;
+        } else {
+            System.out.println("User already exists.");
+            return false;
+        }
+    }
+
+    public boolean removeUser(User user) {
+        // Remove the user if it exists
+        if (users.contains(user)) {
+            users.remove(user);
+            return true;
+        } else {
+            System.out.println("User not found.");
+            return false;
+        }
+    }
+
+    public boolean updateUser(User oldUser, User newUser) {
+        // Update the user if it exists
+        if (users.contains(oldUser)) {
+            int index = users.indexOf(oldUser);
+            users.set(index, newUser);
+            return true;
+        } else {
+            System.out.println("User not found.");
+            return false;
+        }
     }
     
-    /**
-    * @generated
-    */
-    public boolean RemoveUser() {
-        //TODO
-        return false;
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Admin admin = (Admin) obj;
+        return Objects.equals(users, admin.users);
     }
-    
-    /**
-    * @generated
-    */
-    public boolean UpdateUser() {
-        //TODO
-        return false;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(users);
     }
-    
-    
+
+//    @Override
+//    public int compareTo(User otherAdmin) {
+//        // Compare Admin objects based on the number of users they have
+//        return Integer.compare(this.users.size(), otherAdmin.size());
+//    }
+
+    @Override
+    public Admin clone() throws CloneNotSupportedException {
+        // Perform a shallow copy by invoking the Object.clone() method
+		Admin clonedAdmin = (Admin) super.clone();
+		// Create a new ArrayList to avoid sharing the same reference
+		clonedAdmin.users = new ArrayList<>(this.users);
+		return clonedAdmin;
+    }
 }
+

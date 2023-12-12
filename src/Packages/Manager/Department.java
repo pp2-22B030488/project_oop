48547ef93bf3938459e89a7b346c7750b88609ff
Yerkosha <118683459+Yerkosha@users.noreply.github.com>
2023-12-12 14:56:53 +0600

    Updated Department.java

--- src/Packages/Manager/Department.java
+++ src/Packages/Manager/Department.java
@@ -1,72 +1,68 @@
-package Packages;
+package Manager;
+    
+    import java.util.ArrayList;
+    import java.util.List;
 
+    public class Department extends Manager{
 
-/**
-* @generated
-*/
-public class Department extends ManagerManager {
-    
-    /**
-    * @generated
-    */
-    private Department departmentType;
-    
-    /**
-    * @generated
-    */
-    private Set<Teacher> facultyMembers;
-    
-    /**
-    * @generated
-    */
-    private List coursesOffered;
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Department getDepartmentType() {
-        return this.departmentType;
-    }
-    
-    /**
-    * @generated
-    */
-    public Department setDepartmentType(Department departmentType) {
-        this.departmentType = departmentType;
-    }
-    
-    /**
-    * @generated
-    */
-    public Set<Teacher> getFacultyMembers() {
-        return this.facultyMembers;
-    }
-    
-    /**
-    * @generated
-    */
-    public Set<Teacher> setFacultyMembers(Set<Teacher> facultyMembers) {
-        this.facultyMembers = facultyMembers;
-    }
-    
-    /**
-    * @generated
-    */
-    public List getCoursesOffered() {
-        return this.coursesOffered;
-    }
-    
-    /**
-    * @generated
-    */
-    public List setCoursesOffered(List coursesOffered) {
-        this.coursesOffered = coursesOffered;
-    }
-    
+        private Set<Teacher> facultyMembers;
+        private List<Course> coursesOffered;
+        
+        public enum departmentType {
+        	SCIENCE_INNOVATION,
+    	    BUSINESS_DEVELOPMENT,
+    	    STUDENT_AFFAIRS;
+        }
+        
+        
 
-    //                          Operations                                  
-    
-    
-}
+        public Department() {
+            this.facultyMembers = new HashSet<>();
+            this.coursesOffered = new ArrayList<>();
+        }
+
+        public Set<Teacher> getFacultyMembers() {
+            return this.facultyMembers;
+        }
+
+        public void setFacultyMembers(Set<Teacher> facultyMembers) {
+            this.facultyMembers = facultyMembers;
+        }
+
+        public List<Course> getCoursesOffered() {
+            return this.coursesOffered;
+        }
+
+        public void setCoursesOffered(List<Course> coursesOffered) {
+            this.coursesOffered = coursesOffered;
+        }
+        
+        
+        public Department getDepartmentType() {
+            return this.departmentType;
+        }
+          
+        public Department setDepartmentType(Department departmentType) {
+            this.departmentType = departmentType;
+        }
+        
+        
+
+        
+        public void addCourseOffered(Course course) {
+            this.coursesOffered.add(course);
+        }
+
+        
+        public void viewCoursesOffered() {
+            for (Course course : this.coursesOffered) {
+                System.out.println(course.getName()); 
+            }
+        }
+
+      
+        public void removeCourseOffered(Course course) {
+            this.coursesOffered.remove(course);
+        }
+
+    }

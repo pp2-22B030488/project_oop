e698498abc7a6d28d79fee91f72b6f49f828c158
Yerkosha <118683459+Yerkosha@users.noreply.github.com>
2023-12-12 15:00:36 +0600

    Update SchoolManager.java

--- src/Packages/FromEmployee/SchoolManager.java
+++ src/Packages/FromEmployee/SchoolManager.java
@@ -1,80 +1,60 @@
-package Packages;
+package FromEmployee;
 
 
-/**
-* @generated
-*/
-public class SchoolManager extends ManagerManager {
-    
-    /**
-    * @generated
-    */
-    private School scholl;
-    
-    /**
-    * @generated
-    */
-    private Set<Requests> listOfRequestsSchool;
-    
-    /**
-    * @generated
-    */
-    private List<> staffMembers;
-    
-    
-    
-    /**
-    * @generated
-    */
-    public School getScholl() {
-        return this.scholl;
-    }
-    
-    /**
-    * @generated
-    */
-    public School setScholl(School scholl) {
-        this.scholl = scholl;
-    }
-    
-    /**
-    * @generated
-    */
-    public Set<Requests> getListOfRequestsSchool() {
-        return this.listOfRequestsSchool;
-    }
-    
-    /**
-    * @generated
-    */
-    public Set<Requests> setListOfRequestsSchool(Set<Requests> listOfRequestsSchool) {
-        this.listOfRequestsSchool = listOfRequestsSchool;
-    }
-    
-    /**
-    * @generated
-    */
-    public List<> getStaffMembers() {
-        return this.staffMembers;
-    }
-    
-    /**
-    * @generated
-    */
-    public List<> setStaffMembers(List<> staffMembers) {
-        this.staffMembers = staffMembers;
-    }
-    
+import java.util.ArrayList;
+import java.util.List;
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public Course approveCourseAddition() {
-        //TODO
-        return null;
+public class SchoolManager extends Manager{
+
+    private SchoolName school;
+    private List<Request> listOfRequestsSchool;
+    private List<StaffMember> staffMembers;
+
+    public enum SchoolName {
+    	 SITE, SG, BS, ISE, KMA, SAM, SCE
     }
+
+    public SchoolManager(SchoolName school) {
+        this.school = school;
+        this.listOfRequestsSchool = new ArrayList<>();
+        this.staffMembers = new ArrayList<>();
+    }
+
+   
+    public void manageEvents() {
+    	System.out.println("Events managed for " + school);
+    }
+
+    public void addStaffMembers(StaffMember staffMember) {
+        staffMembers.add(staffMember);
+        System.out.println("Staff member added: " + staffMember.getName());
+    }
+
     
+    public void removeStaffMembers(StaffMember staffMember) {
+        staffMembers.remove(staffMember);
+        System.out.println("Staff member removed: " + staffMember.getName());
+    }
+
     
+    public void viewStaffMembers() {
+        for (StaffMember staffMember : staffMembers) {
+            System.out.println(staffMember);
+        }
+    }
+
+    
+    public void approveCourseAddition(Request request) {
+        
+        listOfRequestsSchool.remove(request);
+        System.out.println("Course addition approved for " + request.getCourse().getName());
+    }
+
+    
+    public void sendSchoolReports() {
+        
+        System.out.println("School reports sent for " + school);
+    }
+
+  
 }
